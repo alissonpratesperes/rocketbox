bb188b9216f7e799679232da12b68b067b669cfc
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-30 16:10:15 -0300

    :heavy_plus_sign: Instalação de nova Dependência - MULTER no Back-End da Aplicação.
    Instalada nova Dependência - MULTER, para a tratativa de Upload de Arquivos nas Requisições realizadas;
    Implementadas as devidas configurações da Multer em "./backend/src/config/multer.js  onde a Multer foi configurada para Salvar em Disco os Arquivos upados em "./backend/temp" (adicionado o arquivo ".gitkeep" para o GitHub manter essa pasta, mesmo que vazia, facilitando na hora do clone posterior do Repositório), no Back-End da Aplicação.

---
+++ backend/src/config/multer.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const multer = require('multer');
+const crypto = require('crypto');
+
+    module.exports = {
+        destination: path.resolve(__dirname, '..', '..', 'temp'),
+            storage: multer.diskStorage({
+                destination: (request, file, callback) => {
+                    callback(null, path.resolve(__dirname, '..', '..', 'temp'))
+                },
+                    filename: (request, file, callback) => {
+                        crypto.randomBytes(16, (error, hash) => {
+                            if(error) {
+                                callback(error);
+                            }
+                                file.key = `${hash.toString('hex')}-${file.originalname}`;
+                                    callback(null, file.key);
+                        })
+                    }
+            })
+    };
