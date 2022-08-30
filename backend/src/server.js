e712e9478e647e9e1ccbd16f4cc2764d32b5f450
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-30 14:49:38 -0300

    :card_file_box: Instanciação do MONGOOSE no Back-End da Aplicação.
    Instanciado o MONGOOSE e utilizado o Método ".connect" para realizar a conexão com o Banco de Dados - MongoDB - "omnistack6" já com a String de Conexão com esse DB passada como primeiro parâmetro, no Back-End da Aplicação.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-    app.use(express.json());
-    app.use(express.urlencoded({
-        extended: true
-    }));
-    app.use(require('./routes'));
-    app.listen(3333);
+    mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack6?retryWrites=true', {
+        useNewUrlParser: true
+    });
+
+        app.use(express.json());
+        app.use(express.urlencoded({
+            extended: true
+        }));
+        app.use(require('./routes'));
+        app.listen(3333);
