18132ce155eb62bf6103ce25c1296e1c0a1a7042
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-30 17:18:12 -0300

    :sparkles: Criação do Segundo Controller - "FILECONTROLLER" no Back-End da Aplicação.
    Implementado o Segundo Controller - "FILECONTROLLER", em formato de Classe, que será responsável por encontrar o Box relacionado (que está recebendo o Arquivo, e que é informado nos Parâmetros da Rota referente), criar o Arquivo (recebendo o "title" e "path" da "request.file" e armazenando na Constante "file") e relacionando os dois, através do Push de "file" (que é a Constante com as informações de "title" e "path") no Array de Arquivos do Box, e armazenando no Banco de Dados;
    Em "./backend/src/routes.js", importado o Arquivo de Configuração da Multer criado anteriormente e configurada a Rota que antes era "/boxes" agora para utilizar as configurações especificadas para a Multer e receber o ID de cada Box pelos Parâmetros da Requisição, ficando então: "/boxes/:id/files", à medida que os Arquivos são Upados, irão para Boxes específicos, no Back-End da Aplicação.

---
+++ backend/src/controllers/FileController.js
@@ -0,0 +1,19 @@
+const Box = require('../models/Box');
+const File = require('../models/File');
+
+    class FileController {
+        async store(request, response) {
+            const box = await Box.findById(
+                request.params.id
+                );
+            const file = await File.create({
+                title: request.file.originalname,
+                path: request.file.key
+            });
+                box.files.push(file);
+                    await box.save();
+                        return response.json(file);
+        }
+    }
+
+        module.exports = new FileController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
+const multer = require('multer');
 const express= require('express');
-
 const routes = express.Router();
 
+const multerConfig = require('./config/multer');
 const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
 
     routes.post('/boxes', BoxController.store);
+    routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
 
         module.exports = routes;
