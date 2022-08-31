ffca6ff80ff22c206c8d5935234b82724e9c4caa
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-31 13:05:39 -0300

    💄 Novo Component - BOX no Front-End da Aplicação.
    Criado novo Componente - BOX para a Página do Box (Box Page) da Aplicação, no diretório "./frontend/src/pages" que irá conter todas as Páginas da Aplicação;
    Já implementada a Integração dessa nova Página do Box com o Back-End, carregando as principais propriedades do Box acessado, como: title, files e timestamp (de cada arquivo) que foi upado para a Box;
    Instaladas novas Dependências - DATE-FNS (para lidar com os timestamps dos arquivos) e REACT-ICONS para a utilização de Ícones Representativos dos arquivos em tela;
    Já implementada a Estilização CSS Proprietária, sem fugir muito do Escopo do Curso, no Front-End da Aplicação.

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
+import {distanceInWords} from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
+import {MdInsertDriveFile} from 'react-icons/md';
+import logo from '../../assets/logo.svg';
 import './styles.css';
+import api from '../../services/api';
 
     export default class Box extends Component {
-        render() {
-            return (
-                <h1> Box Page </h1>
-            );
-        }
+        state = {
+            box: {}
+        };
+
+            async componentDidMount() {
+                const box = this.props.match.params.id;
+                const response = await api.get(`/boxes/${box}`);
+                    this.setState({
+                        box: response.data
+                    });
+            }
+
+
+            render() {
+                return (
+                    <div id="box_container">
+                        <header>
+                            <img src={logo} alt="RocketBox"/>
+                                <h1>{this.state.box.title}</h1>
+                        </header>
+                            <ul>
+                                {this.state.box.files && this.state.box.files.map(file => (
+                                    <li>
+                                        <a className="fileInfo" href={file.url}>
+                                            <MdInsertDriveFile size={24} color="A5CFFF"/>
+                                                <strong>{file.title}</strong>
+                                        </a>
+                                            <span>há {distanceInWords(file.createdAt, new Date(), {locale: pt})}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                    </div>
+                );
+            }
     }
