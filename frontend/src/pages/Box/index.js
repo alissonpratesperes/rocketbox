9f827d16c684e0680c7bb695bd15b4727febdf07
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-31 13:24:38 -0300

    ➕ Instalação de nova Dependência - REACT-DROPZONE no Front-End da Aplicação.
    Instalada nova Dependência - REACT-DROPZONE, que irá disponibilizar o Componente - "Dropzone" do React para que seja possível lidar como Upload dos Arquivos para cada Box criada na Aplicação;
    Na Página - Box, em "./frontend/src/pages/Box", implementada a Função "handleUpload" que será executada automaticamente quando forem Selecionados ou Arrastados os arquivos para dentro do Input, que foi disponibilizado com a implementação do Componente - "Dropzone" nesta Página, no Front-End da Aplicação.

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Dropzone from 'react-dropzone';
 import {distanceInWords} from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
@@ -19,27 +20,42 @@ import api from '../../services/api';
                         box: response.data
                     });
             }
+            handleUpload = (files) => {
+                    files.forEach((file) => {
+                        const box = this.props.match.params.id;
+                        const fileData = new FormData();
+                            fileData.append('file', file);
+                                api.post(`/boxes/${box}/files`, fileData);
+                    });
+            };
 
-
-            render() {
-                return (
-                    <div id="box_container">
-                        <header>
-                            <img src={logo} alt="RocketBox"/>
-                                <h1>{this.state.box.title}</h1>
-                        </header>
-                            <ul>
-                                {this.state.box.files && this.state.box.files.map(file => (
-                                    <li>
-                                        <a className="fileInfo" href={file.url}>
-                                            <MdInsertDriveFile size={24} color="A5CFFF"/>
-                                                <strong>{file.title}</strong>
-                                        </a>
-                                            <span>há {distanceInWords(file.createdAt, new Date(), {locale: pt})}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                    </div>
-                );
-            }
+                render() {
+                    return (
+                        <div id="box_container">
+                            <header>
+                                <img src={logo} alt="RocketBox"/>
+                                    <h1>{this.state.box.title}</h1>
+                            </header>
+                                <Dropzone onDropAccepted={this.handleUpload}>
+                                    {({getRootProps, getInputProps}) => (
+                                        <div className="upload" {...getRootProps()}>
+                                            <input {...getInputProps()}/>
+                                                <p>Arraste arquivos ou clique aqui</p>
+                                        </div>
+                                    )}
+                                </Dropzone>
+                                    <ul>
+                                        {this.state.box.files && this.state.box.files.map(file => (
+                                            <li key={file._id}>
+                                                <a className="fileInfo" href={file.url}>
+                                                    <MdInsertDriveFile size={24} color="A5CFFF"/>
+                                                        <strong>{file.title}</strong>
+                                                </a>
+                                                    <span>há {distanceInWords(file.createdAt, new Date(), {locale: pt})}</span>
+                                            </li>
+                                        ))}
+                                    </ul>
+                        </div>
+                    );
+                }
     }
