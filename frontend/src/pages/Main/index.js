d732e5a4a8c2d904a37565fbf15757b0b72df83c
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-30 22:10:37 -0300

    🗃️ Integração da Página - MAIN do Front-End com o Back-End da Aplicação.
    Realizada a Integração da Página - MAIN com o Formulário de Cadastro de Boxes com o Back-End da Aplicação, já criando o Box dentro do Banco de Dados do MongoDB e retornando os dados referentes, em "response.data".

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -2,17 +2,35 @@ import React, {Component} from 'react';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
+import api from '../../services/api';
 
     export default class Main extends Component {
-        render() {
-            return (
-                <div id="main_container">
-                    <form>
-                        <img src={logo} alt="RocketBox"/>
-                            <input type="text" placeholder="criar box de arquivos"/>
-                                <button type="Submit">Criar Box</button>
-                    </form>
-                </div>
-            );
-        }
+        state = {
+            newBox: ''
+        };
+
+            handleInputChange = (event) => {
+                this.setState({
+                    newBox: event.target.value
+                });
+            };
+            handleSubmit = async (event) => {
+                event.preventDefault();
+                    const response = await api.post('/boxes', {
+                        title: this.state.newBox
+                    });
+                        console.log(response.data);
+            };
+
+                render() {
+                    return (
+                        <div id="main_container">
+                            <form onSubmit={this.handleSubmit}>
+                                <img src={logo} alt="RocketBox"/>
+                                    <input type="text" placeholder="criar box de arquivos" value={this.state.newBox} onChange={this.handleInputChange}/>
+                                        <button type="Submit">Criar Box</button>
+                            </form>
+                        </div>
+                    );
+                }
     }
