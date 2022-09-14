28f18cf1b877ba62188d9f636f585ebbf3523e85
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-14 11:02:11 -0300

    :sparkles: Implementação do Component - "Main" no Mobile da Aplicação.
    Implementado o Componente - "Main" no Mobile, bem como a Estilização CSS de acordo com o Escopo do Curso, que está sendo seguido à risca;
    Realizada a criação do diretório "./mobile/src/assets" e o Upload dos arquivos do Logotipo da Aplicação, no Mobile da Aplicação.

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
+
+import logo from '../../assets/logo.png';
+import styles from "./styles";
 
     export default class Main extends Component {
         render() { 
             return (
-                <View>
-                    <Text>
-                        Hello, World! Bitch
-                    </Text>
+                <View style={ styles.container }>
+                    <Image
+                        style={ styles.logo }
+                        source={ logo }
+                    />
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="criar box de arquivos"
+                            placeholderTextColor="#999999"
+                            autoCapitalize="none"
+                            autoCorrect={ false }
+                            underlineColorAndroid="transparent"
+                        />
+                            <TouchableOpacity
+                                onPress={ () => {} }
+                                style={ styles.button }
+                            >
+                                <Text style={ styles.buttonText }>
+                                    Criar Box
+                                </Text>
+                            </TouchableOpacity>
                 </View>
             );
         }
--- mobile/src/pages/Main/styles.js
+++ mobile/src/pages/Main/styles.js
@@ -2,6 +2,44 @@ import { StyleSheet } from "react-native";
 
     const styles = StyleSheet.create({
 
+        container: {
+            paddingHorizontal: 30,
+            flex: 1,
+            alignItems: "stretch",
+            justifyContent: "center"
+        },
+
+        logo: {
+            alignSelf: "center"
+        },
+
+        input: {
+            marginTop: 30,
+            paddingHorizontal: 20,
+            height: 48,
+            borderWidth: 1,
+            borderColor: "#DDDDDD",
+            borderRadius: 4,
+            fontSize: 16
+        },
+
+        button: {
+            marginTop: 10,
+            paddingHorizontal: 20,
+            height: 48,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 4,
+            fontSize: 16,
+            backgroundColor: "#7159C1"
+        },
+
+        buttonText: {
+            fontSize: 16,
+            color: "#FFFFFF",
+            fontWeight: "bold"
+        }
+        
     });
 
         export default styles;
