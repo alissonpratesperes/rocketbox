f8c5f82b186066adffb58e357f0c24b865b17a12
Alisson Prates Peres <a.pratesperes@gmail.com>
2022-09-14 11:15:01 -0300

    ➕ Instalação de nova Dependência - AXIOS no Mobile da Aplicação.
    Instalada nova Dependência - AXIOS, que servirá para o consumo de dados da API pela camada do Mobile;
    Seguindo a mesma estrutura de configuração utilizada na camada do Front-End, o que difere apenas é o endereço de conexão da "baseURL" do AXIOS, que agora aponta para o IP de onde virão os dados da API, visto que o acesso será feito via Device Externo, e não via Emulador, no Mobile da Aplicação;
    Já realizada a integração da Página - MAIN do Mobile com o Back-End, possibilitando o funcionamento da Criação dos Boxes no Banco de Dados, no Mobile da Aplicação.

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -3,32 +3,55 @@ import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 
 import logo from '../../assets/logo.png';
 import styles from "./styles";
+import api from "../../services/api";
 
     export default class Main extends Component {
-        render() { 
-            return (
-                <View style={ styles.container }>
-                    <Image
-                        style={ styles.logo }
-                        source={ logo }
-                    />
-                        <TextInput
-                            style={ styles.input }
-                            placeholder="criar box de arquivos"
-                            placeholderTextColor="#999999"
-                            autoCapitalize="none"
-                            autoCorrect={ false }
-                            underlineColorAndroid="transparent"
-                        />
-                            <TouchableOpacity
-                                onPress={ () => {} }
-                                style={ styles.button }
-                            >
-                                <Text style={ styles.buttonText }>
-                                    Criar Box
-                                </Text>
-                            </TouchableOpacity>
-                </View>
-            );
-        }
+        state = {
+            newBox: ''
+        };
+
+            handleSignIn = async () => {
+                const response = await api.post(
+                    '/boxes',
+                        {
+                            title: this.state.newBox
+                        }
+                );
+                    this.props.navigation.navigate("Box");
+            };
+
+                render() { 
+                    return (
+                        <View
+                            style={ styles.container }
+                        >
+                            <Image
+                                style={ styles.logo }
+                                source={ logo }
+                            />
+                                <TextInput
+                                    style={ styles.input }
+                                    placeholder="criar box de arquivos"
+                                    placeholderTextColor="#999999"
+                                    autoCapitalize="none"
+                                    autoCorrect={ false }
+                                    underlineColorAndroid="transparent"
+                                    value={ this.state.newBox }
+                                    onChangeText={ text => this.setState({
+                                        newBox: text
+                                    }) }
+                                />
+                                    <TouchableOpacity
+                                        onPress={ this.handleSignIn }
+                                        style={ styles.button }
+                                    >
+                                        <Text 
+                                            style={ styles.buttonText }
+                                        >
+                                            Criar Box
+                                        </Text>
+                                    </TouchableOpacity>
+                        </View>
+                    );
+                }
     };
